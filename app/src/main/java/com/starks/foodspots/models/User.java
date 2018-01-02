119133d305c699d74c024269be4d2191cbcffad8
Paras Jain <parasjain.jain@outlook.com>
2018-01-02 22:59:39 +0530

    model User

---
+++ app/src/main/java/com/starks/foodspots/models/User.java
@@ -0,0 +1,12 @@
+package com.starks.foodspots.models;
+
+/**
+ * Created by sharda on 02/01/18.
+ */
+
+public class User extends BaseModel {
+    String username;
+    String email;
+    Float credits;
+    Integer numFoodSpots;
+}
