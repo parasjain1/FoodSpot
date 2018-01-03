a3a01a7025f4f1c39324fd35b3908df4fbb53d2e
vshashank7 <vshashank7@outlook.com>
2018-01-03 10:16:21 +0530

    form objects created

--- app/src/main/java/com/starks/foodspots/LoginActivity.java
+++ app/src/main/java/com/starks/foodspots/LoginActivity.java
@@ -1,19 +1,56 @@
 package com.starks.foodspots;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.starks.foodspots.apiservices.responses.LoginResponse;
 import com.starks.foodspots.interfaces.LoginViewAction;
 import com.starks.foodspots.utils.PrefManager;
 
-public class LoginActivity extends Activity implements LoginViewAction{
+import static android.R.id.message;
+
+public class LoginActivity extends Activity implements LoginViewAction {
+
+    TextView registerTextView;
+    EditText emailEditText,passwordEditText;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initViews();
+    }
+
+    void initViews() {
+        registerTextView = (TextView) findViewById(R.id.registerButton);
+        loginButton=(Button) findViewById(R.id.loginButton);
+        emailEditText=(EditText) findViewById(R.id.email);
+        passwordEditText=(EditText) findViewById(R.id.password);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(i);
+                setContentView(R.layout.activity_signup);
+            }
+        });
     }
 
     @Override
@@ -41,8 +78,10 @@ public class LoginActivity extends Activity implements LoginViewAction{
         displayMessage("Network Error: No Internet access.");
     }
 
+
     @Override
     public void onLogin(LoginResponse loginResponse) {
         new PrefManager(this).putToken(loginResponse.getToken());
+
     }
 }
--- app/src/main/java/com/starks/foodspots/SignupActivity.java
+++ app/src/main/java/com/starks/foodspots/SignupActivity.java
@@ -12,9 +12,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Downloader;
+import com.starks.foodspots.apiservices.responses.LoginResponse;
+import com.starks.foodspots.interfaces.LoginViewAction;
+import com.starks.foodspots.interfaces.SignupViewAction;
+import com.starks.foodspots.models.User;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,7 +28,15 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class SignupActivity extends AppCompatActivity {
+import static android.R.id.message;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
+public class SignupActivity extends AppCompatActivity implements SignupViewAction{
+
+    TextView loginButton;
+    EditText nameEditText,emailEditText,passwordEditText,ageEditText;
+    RadioButton maleRadio,femaleRadio;
+    Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +45,70 @@ public class SignupActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        initViews();
+
     }
 
+
+    void initViews(){
+        loginButton = (TextView) findViewById(R.id.btnLinkLogin);
+        nameEditText=(EditText) findViewById(R.id.);
+        emailEditText=(EditText) findViewById(R.id.signupInputEmail);
+        passwordEditText=(EditText) findViewById(R.id.signupInputPassword);
+        ageEditText=(EditText) findViewById(R.id.signupInputAge);
+        maleRadio=(RadioButton) findViewById(R.id.maleRadioBtn);
+        femaleRadio=(RadioButton) findViewById(R.id.femaleRadioBtn);
+        signUpButton=(Button) findViewById(R.id.btnSignup);
+
+        //goto login screen
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(i);
+                setContentView(R.layout.activity_login);
+            }
+        });
+
+        //submit signup request
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public void displayMessage(String message) {
+
+    }
+
+    @Override
+    public void showLoader() {
+
+    }
+
+    @Override
+    public void hideLoader() {
+
+    }
+
+    @Override
+    public void showNetworkTimeoutError() {
+        displayMessage("Network Error: Request Timed out.");
+    }
+
+    @Override
+    public void showNoNetworkException() {
+        displayMessage("Network Error: No Internet access.");
+    }
+
+
+    @Override
+    public void onSignup(User user) {
+
+    }
 }
