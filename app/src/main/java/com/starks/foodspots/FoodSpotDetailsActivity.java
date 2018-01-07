4c7a3fb030fd4e5185b769f8a616bc0ccc074b07
monp97 <monika.pandey97@gmail.com>
2018-01-07 06:37:07 +0530

    food details

--- app/src/main/java/com/starks/foodspots/FoodSpotDetailsActivity.java
+++ app/src/main/java/com/starks/foodspots/FoodSpotDetailsActivity.java
@@ -8,7 +8,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
-public class FoodSpotDetailsActivity extends AppCompatActivity {
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.starks.foodspots.interfaces.OnFoodSpotsReceiveListener;
+import com.starks.foodspots.models.FoodSpot;
+import com.starks.foodspots.models.FoodspotImage;
+import com.starks.foodspots.presenters.FoodSpotsPresenter;
+import com.starks.foodspots.utils.Constants;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FoodSpotDetailsActivity extends BaseActivity implements OnFoodSpotsReceiveListener, BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener{
+
+    SliderLayout sliderLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +32,10 @@ public class FoodSpotDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_food_spot_details);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        initViews();
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,7 +46,74 @@ public class FoodSpotDetailsActivity extends AppCompatActivity {
             }
         });
 
+        Integer foodSpotId = getIntent().getExtras().getInt("foodSpotId");
+        new FoodSpotsPresenter(this).getFoodSpot(foodSpotId+"");
         Toast.makeText(this, getIntent().getExtras().getInt("foodSpotId") + "", Toast.LENGTH_SHORT).show();
     }
 
+    void initViews(){
+        sliderLayout = (SliderLayout) findViewById(R.id.slider);
+    }
+
+    void populateImageSlider(Map<String, String> map){
+        for(String name : map.keySet()){
+            TextSliderView textSliderView = new TextSliderView(this);
+            // initialize a SliderLayout
+            textSliderView
+                    .image(map.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(FoodSpotDetailsActivity.this);
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",name);
+
+            sliderLayout.addSlider(textSliderView);
+        }
+    }
+
+    @Override
+    public void onFoodSpotSearchResult(ArrayList<FoodSpotSuggestion> foodSpots) {
+
+    }
+
+    @Override
+    public void onReceiveFoodSpots(ArrayList<FoodSpot> foodSpots) {
+
+    }
+
+    @Override
+    public void onReceiveFoodSpot(FoodSpot foodSpot) {
+        Map<String, String> map = new HashMap<>();
+        int counter = 0;
+        for(FoodspotImage foodspotImage : foodSpot.getGallery())
+            map.put(counter++ + "", Constants.ip + foodspotImage.getImage());
+        populateImageSlider(map);
+    }
+
+    @Override
+    public void onDelete() {
+
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
 }
